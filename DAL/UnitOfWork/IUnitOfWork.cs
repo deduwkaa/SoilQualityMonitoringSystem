7ae85d8d46a73ff9724f667e19d6e8d6092088d6
deduwkaa <rymar.danyl@lll.kpi.ua>
2025-04-18 23:50:31 +0300

    Updated  UnitOfWork pattern

--- DAL/UnitOfWork/IUnitOfWork.cs
+++ DAL/UnitOfWork/IUnitOfWork.cs
@@ -2,10 +2,11 @@ using DAL.Repositories.Interfaces;
 
 namespace DAL.UnitOfWork;
 
-public interface IUnitOfWork
+public interface IUnitOfWork : IDisposable
 {
+    public IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = false) where TEntity : class;
+    Task SaveChangesAsync();
     IEmployeeRepository Employees { get; }
-    IIndicatorRepository Indicators { get; }
+    IIndicatorRepository Indicators { get;  }
     IReportRepository Reports { get; }
-    Task<int> SaveAsync();
 }
