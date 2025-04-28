15dfef961e3ee69f20eeaa01f453a5bbbc4b145f
deduwkaa <rymar.danyl@lll.kpi.ua>
2025-04-28 19:07:24 +0300

    Fixing bugs related to Entities and MySQL

--- DAL/Entities/Employee.cs
+++ DAL/Entities/Employee.cs
@@ -1,12 +1,12 @@
-using DAL.Enums;
+using CCL.Security.Identity;
 
 namespace DAL.Entities;
 
-public partial class Indicator
+public class Employee
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public double Value { get; set; }
-    public IndicatorType Type { get; set; }
-    public DateTime CollectedDate { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public Role Role { get; set; }
 }
